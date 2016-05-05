cc899f1c5457fbd403661140442bfb0ba01e75d4
Joana Andrade <mj04nd@gmail.com>
2016-05-05 18:00:07 +0100

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,28 @@
+### Downloading data from the course repo and reading it into a data table
+###
+url1<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url1, destfile="household_power_consumption.zip")
+unzip("household_power_consumption.zip")
+file<-"household_power_consumption.txt"
+
+data<-read.table(file,sep=";",na.strings="?",header=TRUE)
+
+# We are only interested in the lines corresponding to 2007-02-01 and 2007-02-02.
+#Extracting the lines corresponding to 2007-02-01
+day1<-subset(data,Date=="1/2/2007")
+#Extracting the lines correponding to 2007-02-02
+day2<-subset(data,Date=="2/2/2007")
+
+data2<-rbind(day1,day2)
+
+# creating a new column datetime by collapsing Date and Time
+# and converting it in a Date object
+data2$datetime <- apply(data2[ ,c("Date","Time")],1 ,paste ,collapse = " " )
+data2$datetime <- as.POSIXct(strptime(data2$datetime, "%d/%m/%Y %H:%M:%S"))
+
+## Generating, annotating and saving plot2
+
+# background was redefined as transparent
+png(file = "plot2.png",bg="transparent",width=480, height=480, units="px")
+plot(data2$Global_active_power~ data2$datetime,type='l',xlab="",ylab="Global Active Power (kilowatts)",bg="transparent")
+dev.off()
